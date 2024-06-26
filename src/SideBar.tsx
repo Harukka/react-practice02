912920f0f3c33e78397532ba7d1a86d2af0af4e1
Harukka <goooonza@gmail.com>
2024-06-26 10:41:34 +0900

    removed old code

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -85,17 +85,6 @@ export const SideBar = (props: Props) => (
                 <Divider /> 
             </List>
         </DrawerList>
-        <select
-            defaultValue="all"
-            onChange={(e) => props.onSort(e.target.value as Filter)}
-        >
-            <option value="all">すべてのタスク</option>
-            <option value="checked">完了したタスク</option>
-            <option value="unchecked">現在のタスク</option>
-            <option value="removed">ゴミ箱</option>
-
-        </select>
-
     </Drawer>
     
 );
